03cca9a774f826a71c936d5a79b833fc5ebbef18
Parth <adokar.parth@gmail.com>
2026-08-31 16:07:16 +0000

    more tests

--- tests/excercise_1_test.py
+++ tests/excercise_1_test.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 from excercises.excercise_1 import transform_employees
 
 spark = SparkSession.builder \
@@ -39,4 +41,103 @@ def test_transform_employees():
     assert result.collect() == expected.collect()
 
 def test_salary_50000_is_excluded():
-    pass
+    test_data = [
+        (1, "Alice", 60000, "IT"),
+        (2, "Bob", 45000, "HR"),
+        (3, "Charlie", 80000, "IT"),
+        (4, "David", 50000, "Sales"),
+        (5, "Eva", 90000, "IT")
+    ]
+
+    columns = ["id", "name", "salary", "dept"]
+
+    input_df = spark.createDataFrame(test_data, columns)
+
+    result = transform_employees(input_df)
+
+    assert result.filter(col("id") == 4).count() == 0
+
+def test_salary_50001_is_medium():
+    test_data = [
+        (1, "Alice", 50001, "IT") 
+    ]
+
+    columns = ["id", "name", "salary", "dept"]
+
+    input_df = spark.createDataFrame(test_data, columns)
+
+    result = transform_employees(input_df)
+
+    expected_data = [
+        (1, "Alice", 50001, "MEDIUM")
+ 
+    ]
+
+    expected_columns = ["id", "name", "salary", "salary_category"]
+
+    expected = spark.createDataFrame(expected_data, expected_columns)
+
+    assert result.collect() == expected.collect()
+
+def test_salary_80000_is_high():
+    test_data = [
+        (3, "Janice", 80000, "Sales") 
+    ]
+
+    columns = ["id", "name", "salary", "dept"]
+
+    input_df = spark.createDataFrame(test_data, columns)
+
+    result = transform_employees(input_df)
+
+    expected_data = [
+        (3, "Janice", 80000, "HIGH")
+ 
+    ]
+
+    expected_columns = ["id", "name", "salary", "salary_category"]
+
+    expected = spark.createDataFrame(expected_data, expected_columns)
+
+    assert result.collect() == expected.collect()
+
+
+def test_salary_79999_is_medium():
+    test_data = [
+        (3, "Kerry", 79999, "Sales") 
+    ]
+
+    columns = ["id", "name", "salary", "dept"]
+
+    input_df = spark.createDataFrame(test_data, columns)
+
+    result = transform_employees(input_df)
+
+    expected_data = [
+        (3, "Kerry", 79999, "MEDIUM")
+ 
+    ]
+
+    expected_columns = ["id", "name", "salary", "salary_category"]
+
+    expected = spark.createDataFrame(expected_data, expected_columns)
+
+    assert result.collect() == expected.collect()
+
+def test_null_salary():
+    test_data = [
+            (3, "Kerry", None, "Sales") 
+        ]
+    
+    schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("name", StringType(), True),
+    StructField("salary", IntegerType(), True),
+    StructField("dept", StringType(), True)
+])
+
+    input_df = spark.createDataFrame(test_data, schema)
+
+    result = transform_employees(input_df)
+
+    assert result.count() == 0
